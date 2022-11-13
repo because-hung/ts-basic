098de880440014e2d6d1973132d0eae2e32be44f
Becasuse-hung <4a33a006@stust.edu.tw>
2022-11-14 04:22:11 +0800

    簡單環境配置

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// --- 基本類型 ---
+let str = 'Barry';
+let str1;
+str1 = "Barry2";
+let num = 1000;
+let boo = true;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLGVBQWU7QUFFZixJQUFJLEdBQUcsR0FBVyxPQUFPLENBQUE7QUFDekIsSUFBSSxJQUFZLENBQUM7QUFDakIsSUFBSSxHQUFHLFFBQVEsQ0FBQTtBQUVmLElBQUksR0FBRyxHQUFXLElBQUksQ0FBQTtBQUN0QixJQUFJLEdBQUcsR0FBWSxJQUFJLENBQUEifQ==
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// --- 基本類型 ---
+
+let str: string = 'Barry'
+let str1: string;
+str1 = "Barry2"
+
+let num: number = 1000
+let boo: boolean = true
+
